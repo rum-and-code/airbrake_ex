1a6e619895a689ef166c6f9e942affd68e6c512e
Etienne Lacoursiere <lacoursiere.etienne@gmail.com>
2024-08-01 14:20:32 -0400

    filter stacktrace

--- lib/airbrake_ex/exception_parser.ex
+++ lib/airbrake_ex/exception_parser.ex
@@ -6,26 +6,26 @@ defmodule AirbrakeEx.ExceptionParser do
   def parse(exception, stacktrace \\ []) do
     filter_params = AirbrakeEx.Config.get(:airbrake_ex, :filter_parameters, [])
     filtered_exception = struct(exception.__struct__, Utils.filter(exception, filter_params))
-    filtered_stacktrace = Utils.filter(stacktrace, filter_params)
 
     %{
       type: exception.__struct__,
       message: Exception.message(filtered_exception),
-      backtrace: stacktrace(filtered_stacktrace)
+      backtrace: stacktrace(stacktrace, filter_params)
     }
   end
 
-  defp stacktrace(stacktrace) do
+  defp stacktrace(stacktrace, filter_params) do
     Enum.map(stacktrace, fn
       {module, function, args, params} ->
         file = Keyword.get(params, :file)
         line_number = Keyword.get(params, :line, 0)
+        filtered_args = args(args, filter_params)
 
         function =
           if file do
-            "#{function}#{args(args)}"
+            "#{function}#{filtered_args}"
           else
-            "#{module}.#{function}#{args(args)}"
+            "#{module}.#{function}#{filtered_args}"
           end
 
         file_path =
@@ -43,11 +43,11 @@ defmodule AirbrakeEx.ExceptionParser do
     end)
   end
 
-  defp args(args) when is_integer(args) do
+  defp args(args, _) when is_integer(args) do
     "/#{args}"
   end
 
-  defp args(args) when is_list(args) do
-    "(#{args |> Enum.map(&inspect(&1)) |> Enum.join(", ")})"
+  defp args(args, filter_params) when is_list(args) do
+    "(#{args |> Enum.map(&inspect(Utils.filter(&1, filter_params))) |> Enum.join(", ")})"
   end
 end
